db90452c13631abfbda8406b7447e4e4c079a9f3
Elliott Leonard <egleonard88@gmail.com>
2022-03-28 17:11:54 -0600

    updates

---
+++ review.js
@@ -0,0 +1,16 @@
+//Function declaration syntax
+
+function funkyFunc(param1, param2) {
+  //logic here
+  return answer;
+}
+
+//Arrow function syntax
+
+const func = (param1) => `this is a parameter: ${param1}`;
+
+const func1 = () => {
+  return param1;
+};
+
+const func2 = (param1, param2) => {};
